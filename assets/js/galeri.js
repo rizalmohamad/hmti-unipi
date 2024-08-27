234100a2fbb8b92ac95e03610156b82e87895416
Rizal Mohamad <rizalmohamad740@gmail.com>
2024-08-28 00:35:43 +0700

    edit kegiatan and add json(galeri)

--- assets/js/galeri.js
+++ assets/js/galeri.js
@@ -1,57 +1,30 @@
-let galeriFoto = [
-  {
-    category: "filter-workshop",
-    image: "assets/img/kegiatan/workshop1.png",
-    name: "Workshop 2022",
-    year: "2022",
-  },
-  {
-    category: "filter-workshop",
-    image: "assets/img/kegiatan/workshop-2.jpg",
-    name: "Workshop 2022",
-    year: "2022",
-  },
-  {
-    category: "filter-workshop",
-    image: "assets/img/kegiatan/workshop-3.jpg",
-    name: "Workshop 2022",
-    year: "2022",
-  },
-  {
-    category: "filter-ldko",
-    image: "assets/img/kegiatan/ldko1.jpg",
-    name: "LDKO 2022",
-    year: "2022",
-  },
-  {
-    category: "filter-ldko",
-    image: "assets/img/kegiatan/ldko3.jpg",
-    name: "LDKO 2022",
-    year: "2022",
-  },
-  {
-    category: "filter-ldko",
-    image: "assets/img/kegiatan/ldk.jpg",
-    name: "LDKO 2022",
-    year: "2022",
-  },
-];
+// Fetch data from datagaleri.json
 
-const card = document.querySelector(".portfolio-container");
-for (let i in galeriFoto) {
-  card.innerHTML += `
-    <div class="col-lg-4 col-md-6 portfolio-item ${galeriFoto[i].category}">
-        <div class="portfolio-wrap">
-            <img src="${galeriFoto[i].image}" class="img-fluid" alt="" />
-            <div class="portfolio-info">
-                <h4>${galeriFoto[i].name}</h4>
-                <p>${galeriFoto[i].year}</p>
-                <div class="portfolio-links">
-                    <a href="${galeriFoto[i].image}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="WorkShop 1"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+fetch("assets/js/datagaleri.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const card = document.querySelector(".portfolio-container");
+    const imageGallery = data.imageGallery;
+
+    imageGallery.forEach((image) => {
+      const { category, pict, name, title } = image;
+
+      card.innerHTML += `
+        <div class="col-lg-4 col-md-6 portfolio-item ${category}">
+            <div class="portfolio-wrap">
+                <img src="${pict}" class="img-fluid" alt="${name}" />
+                <div class="portfolio-info">
+                    <h4>${name}</h4>
+                    <div class="portfolio-links">
+                        <a href="${pict}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="${title}"><i class="bx bx-plus"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-}
+      `;
+    });
+    const lightbox = GLightbox({
+      selector: ".portfolio-lightbox",
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
